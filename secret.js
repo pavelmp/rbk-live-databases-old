70d3a4cd3992f5e028fb48d60be8305a0698b338
Pavel Parshakov <pavelmp@gmail.com>
2019-05-12 09:17:43 +0300

    Add secret.js to repo

---
+++ secret.js
@@ -0,0 +1,8 @@
+//In terminal, type "export SECRET_KEY=somestring, before you run npm start";
+//To see stored environmental value, type "echo $SECRET_KEY"
+//To see all variable, type "env"
+exports.SECRET_KEY = process.env.SECRET_KEY;
+
+
+// exports.SECRET_KEY = 'somestring';
+
